879bc461dd6d35f4fc606a706cf552ae6f177f22
Cryspaulino <pau22008@byui.edu>
2025-01-11 22:23:18 -0800

    yes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your letter percentage?");
+        string stperc = Console.ReadLine();
+        int perc = int.Parse(stperc);
+
+        string letter = "";
+
+        if (perc >= 90)
+        {
+            letter = "A";
+        }
+        else if (perc >= 80)
+
+        {
+            letter = "B";
+        }
+        else if (perc >= 70)
+
+        {
+            letter = "C";
+        }
+        else if (perc >= 60)
+
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter is a: {letter}");
+
+        if (perc >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed.");
+        }
+
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
     }
 }
