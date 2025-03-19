bc2df1c1688bd58e0e77c84950dd826db830240a
Cryspaulino <pau22008@byui.edu>
2025-03-18 20:38:06 -0700

    poly

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("purple", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("blue", 4, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("yellow", 6);
+        shapes.Add(s3);
+
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape are is {area}");
+        }
+
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,31 @@
+public abstract class Shape
+{
+    private string _color;
+
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+    
+
+}
+
+
+//     public double GetArea()
+//     {
+//         return 2.2;
+//     }
+// }
